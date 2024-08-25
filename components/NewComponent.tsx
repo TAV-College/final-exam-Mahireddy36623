64fa989c247e48df91671bc190c984d115db2b71
praneelreddy07 <praneelreddy0509@gmail.com>
2024-08-25 12:26:21 -0400

    Added NewTab component and integrated it into the project

---
+++ components/NewComponent.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const NewComponent: React.FC = () => {
+    return (
+        <div>
+            <h1>This is the New Component</h1>
+            <p>We can dispaly the content of the new component  here.</p>
+        </div>
+    );
+};
+
+export default NewComponent;
